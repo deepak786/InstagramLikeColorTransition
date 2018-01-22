484f970ec6ec02c2026ebc60887a12be76b89579
Deepak <er.deepakgoyal786@gmail.com>
2018-01-22 18:03:55 +0530

    Instagram like color transition

---
+++ README.md
@@ -0,0 +1,19 @@
+# InstagramLikeColorTransition
+
+This is the sample app which shows the color transition animation like in Instagram.
+
+## Demo
+<img src="video/recorded.gif" alt="Screenshot" width="350" />
+
+# License
+
+    Copyright (c) 2018, Deepak and/or its affiliates. All rights reserved.
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions are met:
+    
+    - Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    
+    - Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
---
+++ app/src/test/java/com/sample/instagramlikecolortransition/ExampleUnitTest.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2018, Deepak and/or its affiliates. All rights reserved.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the distribution.
+ */
+
+package com.sample.instagramlikecolortransition;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
